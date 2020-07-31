41800d1caf1b369a69ca61dd20605ed66d315439
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2020-07-31 16:26:15 -0500

    add

---
+++ cluster/deploy.ps1
@@ -0,0 +1,28 @@
+kubectl create namespace "sut"
+
+kubectl apply -f ./deploy-db2.yaml
+
+Start-Sleep 10
+
+kubectl get pods -n sut -l app=pod-db2
+
+Start-Sleep 60
+
+kubectl logs -n sut -l app=pod-db2
+
+$POD = kubectl get pods -n sut -l app=pod-db2 -o name
+$POD = $POD.replace("pod/", "")
+
+Start-Sleep 60
+
+kubectl apply -f ./service-db2.yaml
+
+kubectl get services -n sut
+
+write-host "kubectl port-forward ${POD} -n sut 50000"
+
+# kubectl logs -n joy -l app=pod-hssbp-db2
+
+#kubectl exec -it $POD -n joy -- /bin/bash
+
+
---
+++ cluster/undeploy.ps1
@@ -0,0 +1,4 @@
+kubectl delete -f ./deploy-db2.yaml
+kubectl delete -f ./service-db2.yaml
+
+
