09587d068a7b10cdb68d98689b91f351610b15c3
Gregory Valderrama <gcvalderrama@hotmail.com>
2021-04-09 23:04:11 -0500

    add

--- build.bash
+++ build.bash
@@ -4,5 +4,5 @@ docker-compose build
 
 popd
 
-docker tag sut/db2 localhost:48700/sut/db2
-docker push localhost:48700/sut/db2
+docker tag sut/db2 localhost:32000/sut/db2
+docker push localhost:32000/sut/db2
--- cluster/deploy.ps1
+++ cluster/deploy.ps1
@@ -4,13 +4,13 @@ kubectl apply -f ./deploy-db2.yaml
 
 Start-Sleep 10
 
-kubectl get pods -n sut -l app=pod-db2
+kubectl get pods -n sut -l key=pod-db2
 
 Start-Sleep 60
 
 kubectl logs -n sut -l app=pod-db2
 
-$POD = kubectl get pods -n sut -l app=pod-db2 -o name
+$POD = kubectl get pods -n sut -l key=pod-db2 -o name
 $POD = $POD.replace("pod/", "")
 
 Start-Sleep 60
@@ -21,7 +21,7 @@ kubectl get services -n sut
 
 write-host "kubectl port-forward ${POD} -n sut 50000"
 
-# kubectl logs -n joy -l app=pod-hssbp-db2
+kubectl logs -n sut -l key=pod-db2
 
 #kubectl exec -it $POD -n joy -- /bin/bash
 
